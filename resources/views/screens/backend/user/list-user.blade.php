7399c4c63533e34c4a35ec46644cc49c5e22729a
Nguyen Quang Huy <nquanghuy337@gmail.com>
2023-01-30 20:53:39 +0700

    update

--- resources/views/screens/backend/user/list-user.blade.php
+++ resources/views/screens/backend/user/list-user.blade.php
@@ -113,7 +113,6 @@
                         <th>Trạng thái</th>
                         <th>Thao tác</th>
                         <th>Edit Role</th>
-                        {{-- <th>Edit Permission</th> --}}
                     </tr>
                 </thead>
                 <tbody>
@@ -141,21 +140,23 @@
                             <a class="btn btn-light  btn-sm mr-2" id="change_status">
                                 <i class="ki ki-reload text-warning"></i>
                             </a>
-                            <a href="{{route('admin.user.evaluate', $user->id)}}" class="btn btn-light  btn-sm mr-2">
-                                <i class="ki ki-reload text-warning"></i>
+                            <a href="{{route('admin.user.evaluate', $user->id)}}" title="Đánh giá" class="btn btn-light  btn-sm mr-2">
+                            <span class="svg-icon svg-icon-primary svg-icon-2x"><!--begin::Svg Icon | path:C:\wamp64\www\keenthemes\themes\metronic\theme\html\demo1\dist/../src/media/svg/icons\Communication\Flag.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                            <rect x="0" y="0" width="24" height="24" />
+                                            <path d="M3.5,3 L5,3 L5,19.5 C5,20.3284271 4.32842712,21 3.5,21 L3.5,21 C2.67157288,21 2,20.3284271 2,19.5 L2,4.5 C2,3.67157288 2.67157288,3 3.5,3 Z" fill="#000000" />
+                                            <path d="M6.99987583,2.99995344 L19.754647,2.99999303 C20.3069317,2.99999474 20.7546456,3.44771138 20.7546439,3.99999613 C20.7546431,4.24703684 20.6631995,4.48533385 20.497938,4.66895776 L17.5,8 L20.4979317,11.3310353 C20.8673908,11.7415453 20.8341123,12.3738351 20.4236023,12.7432941 C20.2399776,12.9085564 20.0016794,13 19.7546376,13 L6.99987583,13 L6.99987583,2.99995344 Z" fill="#000000" opacity="0.3" />
+                                        </g>
+                                    </svg><!--end::Svg Icon--></span>
                             </a>
                         </td>
                         <td>
                             <button onclick="javascript:name_edit_role({{ $user }})" class="btn btn-light-primary px-6 font-weight-bold" data-bs-toggle="modal" data-bs-target="#exampleModal">
                                 <i class="flaticon2-gear text-primary"></i>
                             </button>
-
                         </td>
                     </tr>
-
                     @endforeach
-
-
                 </tbody>
             </table>
             <!--end: Datatable-->
